4762aa458737723669ec9ff988188af36a0c76a4
xero7689 <volleyp7689@gmail.com>
2020-03-23 14:29:51 +0800

    Add field to list_display

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Author, Article, Label, Comment
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'add_date')
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'create_date', 'modify_date', 'author', 'visible')
+
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'create_date')
+
 # Register your models here.
-admin.site.register(Author)
-admin.site.register(Article)
-admin.site.register(Label)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Label, LabelAdmin)
